611ed45d4ec65b6f9f219b7fb7c5c5eee3933375
Elena Haran <elenaharan@gmail.com>
2021-08-29 23:52:30 +0100

    Integrated database with API using mongoose

--- index.js
+++ index.js
@@ -1,96 +1,164 @@
 const express = require("express"),
-  morgan = require("morgan"),
-  bodyParser = require("body-parser");
+bodyParser = require("body-parser"),
+uuid = require("uuid"),
+morgan = require("morgan");
+const mongoose = require("mongoose");
+const Models = require('./models.js');
+const app = express();
+mongoose.connect('mongodb://localhost:27017/myFlixDB', {useNewUrlParser: true, useUnifiedTopology: true});
+
+const Movies = Models.Movie;
+const Users = Models.User;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
 const path = require("path");
 const port = process.env.PORT || 8080;
 
-const app = express();
-
 app.use(morgan("common"));
 app.use(express.static("public"));
 
-let topMovies = [
-  {
-    title: "Raya and the Last Dragon",
-    director: ["Don Hall", "Carlos Lopez Estrada"],
-  },
-  {
-    title: "Luca",
-    director: "Enrico Casarosa",
-  },
-  {
-    title: "Coco",
-    director: "Lee Unkrich",
-  },
-  {
-    title: "Abominable",
-    director: "Jill Culton",
-  },
-  {
-    title: "The Terminal",
-    director: "Steven Spielberg",
-  },
-  {
-    title: "The Intern",
-    director: "Nancy Meyers",
-  },
-  {
-    title: "The Holiday",
-    director: "Nancy Meyers",
-  },
-  {
-    title: "The Mauritanian",
-    director: "Kevin Macdonald",
-  },
-  {
-    title: "Eat Pray Love",
-    director: "Ryan Murphy",
-  },
-  {
-    title: "Breakfast at Tiffany's",
-    director: "Blake Edwards",
-  },
-];
-
-//GET Requests
+//Returns ALL movies to the user
 app.get("/movies", (req, res) => {
-  res.send("Successful GET request returning data on all the movies");
+  Movies.find()
+  .then((movies) => {
+    res.status(201).json(movies);
+  })
+  .catch((err) => {
+    console.error(err);
+    res.status(500).send('Error: ' + err);
+  });
 });
 
-app.get("/", (req, res) => {
-  res.send("Welcome to myFlix!");
+//This function returns a single movie by title
+app.get("/movies/:Title", (req, res) => {
+  Movies.findOne({Title: req.params.Title})
+  .then((movies) => {
+    res.json(movies);
+  })
+  .catch((err) => {
+    console.error(err);
+    res.status(500).send('Error: ' + err);
+  });
 });
 
-app.get("/movies/:title", (req, res) => {
-  res.send("Successful GET request returning data about a single movie");
+//Returns data about a genre by name (e.g., "Comedy")
+app.get("/genres/:genre", (req, res) => {
+  Movies.findOne({"Genre.Name": req.params.genre})
+  .then((genre) => {
+    res.json(genre);
+  })
+  .catch((err) => {
+    console.error(err);
+    res.status(500).send('Error: ' + err);
+  });
 });
 
-app.get("/genres/:name", (req, res) => {
-  res.send("Successful GET request returns data about a genre");
+//Returns data about a director by name
+app.get("/directors/:Name", (req, res) => {
+  Movies.findOne({"Director.Name": req.params.Name})
+  .then((director) => {
+    res.json(director);
+  })
+  .catch((err) => {
+    console.error(err);
+    res.status(500).send('Error: ' + err);
+  });
 });
 
-app.get("/directors/:name", (req, res) => {
-  res.send("Successful GET request returns data about a movie director");
-});
-
+//This function allows new users to register
 app.post("/users", (req, res) => {
-  res.send("Successful POST request will add a new user to database");
-  res.end();
+  Users.findOne({ Username: req.body.Username})
+  .then((user) => {
+    if (user) {
+      return res.status(400).send(req.body.Username + ' already exists');
+    } else {
+      Users.create({
+        Username: req.body.Username,
+        Password: req.body.Password,
+        Email: req.body.Email,
+        Birthday: req.body.Birthday
+      })
+      .then((user) => {res.status(201).json(user)})
+      .catch((error) => {
+        console.error(error);
+        res.status(500).send('Error: ' + error);
+      })
+    }
+  })
+  .catch((error) => {
+    console.error(error);
+    res.status(500).send('Error: ' + error);
+  });
 });
 
-app.delete("/users/:username", (req, res) => {
-  res.send("Successful DELETE request will allow users to deregister");
+//Updates user info
+app.put("/users/update/:Username", (req, res) => {
+  Users.findOneAndUpdate({Username: req.params.Username},
+    { $set:
+    {
+      Username: req.body.Username,
+      Password: req.body.Password,
+      Email: req.body.Email,
+      Birthday: req.body.Birthday
+    },
+  },
+  {new: true},
+  (err, updatedUser) => {
+    if(err) {
+      console.error(err);
+      res.status(500).send('Error: ' + err);
+    } else {
+      res.json(updatedUser);
+    }
+  });
 });
 
-app.patch("users/:username", (req, res) => {
-  res.send("Successful Patch request will allow users update their info");
+//Adds a movie to a user's list of favorites
+app.post("/users/:Username/movies/:MovieID", (req, res) => {
+  Users.findOneAndUpdate({Username: req.params.Username},
+    {$push: {FavoriteMovies: req.params.MovieID}},
+    {new: true},
+    (err, updatedUser) => {
+      if(err) {
+        console.error(err);
+        res.status(500).send('Error: ' + err);
+      } else {
+        res.json(updatedUser);
+      }
+    });
 });
 
-//handles errors
-app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).send("Something broke!");
+//Removes a movie from user's list of favorites
+app.delete("/users/:Username/movies/:MovieID", (req, res) => {
+  Users.findOneAndUpdate({Username: req.params.Username},
+    {$pull: {FavoriteMovies: req.params.MovieID}},
+    {new: true},
+    (err, updatedUser) => {
+      if(err) {
+        console.error(err);
+        res.status(500).send('Error: ' + err);
+      } else {
+        res.json(updatedUser);
+      }
+    });
+});
+
+//Allows existing users to deregister
+app.delete('/users/:Username', (req, res) => {
+  Users.findOneAndRemove({Username: req.params.Username})
+  .then((user) => {
+    if(!user) {
+      res.status(400).send(req.params.Username + ' was not found');
+    } else {
+      res.status(200).send(req.params.Username + ' was deleted.');
+    }
+  })
+  .catch((err) => {
+    console.error(err);
+    res.status(500).send('Error: ' + err);
+  });
 });
 
 //serves static file
