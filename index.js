3925aa394080a56207d72554d8dd1b2aa57bdecf
Elena Haran <elenaharan@gmail.com>
2021-09-30 12:11:52 +0100

    Added a GET single user method

--- index.js
+++ index.js
@@ -65,6 +65,20 @@ app.get("/users", passport.authenticate("jwt", { session: false }),
 }
 );
 
+//Get a single user
+app.get("/users/:Username", passport.authenticate("jwt", {session: false}),
+(req, res) => {
+  Users.findOne({ Username: req.params.Username })
+  .then((user) => {
+    res.json(user);
+  })
+  .catch((err) => {
+    console.error(err);
+    res.status(500).send("Error: " + err);
+  });
+}
+);
+
 //This function returns a single movie by title
 app.get(
   "/movies/:Title",
